9191c7620b6b19ef333156628c037faa911c61c7
deasana <demchuk.oksana.w@gmail.com>
2026-03-18 13:49:32 +0100

    feat: Implemented a login and register system

---
+++ routes/users.js
@@ -0,0 +1,46 @@
+const router = require('express').Router();
+const User = require('../models/User');
+const bcrypt = require('bcrypt');
+
+//register
+router.post('/register', async (req, res) => {
+    try{
+        //generate a new password
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
+        //create a new user
+        const newUser = new User({
+            username: req.body.username,
+            email: req.body.email,
+            password: hashedPassword
+        });
+
+        //save user and respond
+        const user = await newUser.save();
+        res.status(201).json(user._id);
+    } catch (err) {
+        res.status(400).json({ message: err.message });
+    }
+});
+
+//login
+router.post('/login', async (req, res) => {
+    try {
+        const user = await User.findOne({
+            email: req.body.email
+        });
+        if (!user) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+        const isMatch = await bcrypt.compare(req.body.password, user.password);
+        if (!isMatch) {
+            return res.status(400).json({ message: 'Invalid credentials' });
+        }
+        res.status(200).json({ _id: user._id, username: user.username });
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+});
+
+module.exports = router;
