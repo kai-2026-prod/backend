8fa56a8f41d32c15f1d9ab25a259a378862ca98f
d-sarapaniuk <d.sarapaniuk@gmail.com>
2026-04-07 13:33:25 +0300

    Added ESLint config

---
+++ eslint.config.js
@@ -0,0 +1,17 @@
+const js = require("@eslint/js");
+
+module.exports = [
+  js.configs.recommended,
+  {
+    files: ["**/*.js"],
+    ignores: ["node_modules/**"],
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "commonjs", // change to "module" if you use import/export
+    },
+    rules: {
+      "no-unused-vars": "warn",
+      "no-undef": "error",
+    },
+  },
+];
